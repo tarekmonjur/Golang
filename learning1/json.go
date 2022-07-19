2292d3390b58bd23a34c8af5ce78f59d4853a7c1
tarekmonjur <tarekmonjur@gmail.com>
2022-07-20 00:23:06 +0600

    learn request&json

--- learning1/json.go
+++ learning1/json.go
@@ -9,7 +9,7 @@ type posts struct {
 	Title  string   `json:"title"`
 	Body   string   `json:"body"`
 	UserId int      `json:"-"`
-	Tags   []string `json:"tags","omitempty"`
+	Tags   []string `json:"tags,omitempty"`
 }
 
 func main() {
@@ -18,10 +18,36 @@ func main() {
 		{"title one", "body...sadfaf", 1, []string{"tag1", "tag2"}},
 		{"title two", "body...here", 2, nil},
 	}
-	// jsonData, err := json.Marshal(postData)
-	jsonData, err := json.MarshalIndent(postData, "", "\t")
+	encodeDataB := EncodeJson(postData)
+	// decodeData := DecodeJson(encodeDataB).([]posts)
+	decodeData := DecodeJson(encodeDataB)
+	fmt.Println(decodeData[0])
+
+}
+
+func EncodeJson(data interface{}) []byte {
+	jsonData, err := json.Marshal(data)
+	// jsonData, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", jsonData)
+	// fmt.Println(string(jsonData))
+	// fmt.Printf("%s\n", jsonData)
+	return jsonData
+}
+
+func DecodeJson(data []byte) []interface{} {
+	var newJson []interface{}
+	// var newJson []any
+	// var newJson []posts
+	isValid := json.Valid(data)
+	if isValid {
+		json.Unmarshal(data, &newJson)
+		// fmt.Println(newJson)
+		// fmt.Printf("%#v\n", newJson)
+		// fmt.Println(newJson[0].Title)
+	} else {
+		fmt.Println("Json was not valid")
+	}
+	return newJson
 }
